f3b0f590ee7c0a078d507dea97008bcc5c7e5cae
shubhu014 <95476722+shubhu014@users.noreply.github.com>
2022-01-27 18:15:25 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 #
 file '/myfile' do
-  content 'Checking after 2 hours'
+  content 'Its working... The default.rb cloned from Github repo'
 action :create
 end
 
