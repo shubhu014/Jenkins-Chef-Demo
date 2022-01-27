5d1a1c29cb41876a8bc0edcd24bf0f4137090ec1
shubhu014 <95476722+shubhu014@users.noreply.github.com>
2022-01-27 20:00:03 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 #
 file '/myfile' do
-  content 'Testing if it is working fine2'
+  content 'Testing if it is working fine3'
 action :create
 end
 
