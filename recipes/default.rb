032efeaab5edb9f7a54eae1114be5fb98cd9ef6c
shubhu014 <95476722+shubhu014@users.noreply.github.com>
2022-01-27 15:53:45 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,34 @@
-jdjf
+#
+# Cookbook:: test-cookbook
+# Recipe:: test1-recipe
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#
+file '/myfile' do
+  content 'Checking after 2 hours'
+action :create
+end
+
+execute "run a script" do
+  command <<-EOH
+  touch /ShubhamFile
+  EOH
+end
+
+user "Simran" do
+  action :create
+end
+
+group "DataGeekGroup" do
+  action :create
+  members 'Simran'
+  append true
+end
+
+user 'Raj'
+
+group "DataGeekGroup" do
+  action :create
+  members 'Raj'
+  append true
+end
