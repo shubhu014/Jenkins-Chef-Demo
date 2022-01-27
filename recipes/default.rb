9ce5c546a366065a3dd89549c6464594c75812eb
shubhu014 <95476722+shubhu014@users.noreply.github.com>
2022-01-27 22:10:41 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 #
 file '/myfile' do
-  content 'Checking.......Now Chef workstation will also updated.'
+  content '-c added in chef client command ......... Checking.......Now Chef workstation will also updated.'
 action :create
 end
 
