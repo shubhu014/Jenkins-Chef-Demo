7cd7e22413c81f5f6d1bee58c3da3f13096c7ee3
shubhu014 <95476722+shubhu014@users.noreply.github.com>
2022-01-28 10:46:20 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 #
 file '/myfile' do
-  content 'removed 3rd step......... Checking.......Now Chef workstation will also updated.'
+  content 'Now both the nodes are being updated automatically'
 action :create
 end
 
